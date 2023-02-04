4706b206e151efa06dedda2872727ed08b795a7a
VitaliiHavryliuk <107536476+VitaliiHavryliuk@users.noreply.github.com>
2023-02-04 20:56:15 +0200

    v1

---
+++ Lesson3API/Add.cs
@@ -0,0 +1,52 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using Models.Output;
+using Models.Input;
+
+namespace Lesson3API
+{
+    public static class Add
+    {
+        [FunctionName("Add")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            [CosmosDB(databaseName:"beer-db",
+                containerName:"beer-container",
+                Connection = "DBConnection")]
+                IAsyncCollector<Beer> output,
+            ILogger log)
+        {
+            log.LogInformation($"Add function has started!");
+            var body = await new StreamReader(req.Body).ReadToEndAsync();
+            
+            CreateBeer input;
+            try
+            {
+                input = JsonSerializer.Deserialize<CreateBeer>(body);
+            }
+            catch (Exception ex)
+            {
+                log.LogError($"Excetion occured in method Add with message: {ex.Message}");
+                return new BadRequestObjectResult(ex);
+            }
+
+            var newBeer = new Beer
+            {
+                Description = input.Description,
+                Name = input.Name,
+                Email = req.HttpContext?.User?.Identity?.Name,
+                Id = Guid.NewGuid()
+            };
+
+            await output.AddAsync(newBeer);
+            return new OkObjectResult(newBeer);
+        }
+    }
+}
