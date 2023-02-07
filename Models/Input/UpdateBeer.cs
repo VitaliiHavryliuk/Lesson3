9b70c655196c8337331c3438a25cc2f02a63a398
VitaliiHavryliuk <107536476+VitaliiHavryliuk@users.noreply.github.com>
2023-02-07 18:53:08 +0200

    Update

--- Models/Input/UpdateBeer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Models.Input
-{
-    public class UpdateBeer
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-}
